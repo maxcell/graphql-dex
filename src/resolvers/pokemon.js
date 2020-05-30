e483b87b57864b279737eda0eae7b21b6eb1b7f4
adammc331 <amcneilly331@gmail.com>
2020-05-30 16:41:07 -0400

    Adding sprites to pokemon resolver for #15.

--- src/resolvers/pokemon.js
+++ src/resolvers/pokemon.js
@@ -1,8 +1,12 @@
+const spriteBase = "https://raw.githubusercontent.com/maxcell/graphql-dex/master/db/seeds/sprites/";
+const spriteExtension = ".png"
+
 export default {
   Pokemon: {
     types: (pokemon, _, { dataSources }) =>
       dataSources.pokemonAPI.getTypesForPokemon(pokemon.id),
     abilities: (pokemon, _, { dataSources }) =>
-      dataSources.pokemonAPI.getAbilitiesForPokemon(pokemon.id)
+      dataSources.pokemonAPI.getAbilitiesForPokemon(pokemon.id),
+    sprite: (pokemon, _, __, ) => spriteBase.concat(pokemon.id).concat(spriteExtension)
   }
 };
